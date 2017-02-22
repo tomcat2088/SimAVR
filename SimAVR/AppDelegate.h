7b9cad8fe9fe744f4df93b5d562cc9fb20658632
tomcat2088 <tomcat1991@126.com>
2017-02-22 10:21:00 +0800

    Initial Commit

---
+++ SimAVR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimAVR
+//
+//  Created by wangyang on 2017/2/22.
+//  Copyright © 2017年 wangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
